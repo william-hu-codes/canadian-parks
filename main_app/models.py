e524625d359d0e59a0c716bbe61a9e2e0a66fd20
william-hu-codes <william.hu307@gmail.com>
2023-09-21 12:03:13 -0400

    migrated new model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class National_park(models.Model):
+    name = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
